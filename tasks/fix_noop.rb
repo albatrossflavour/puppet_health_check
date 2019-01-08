369f09c10c0c026c4b33cf175542b4a3ac292bfd
Tony Green <tgreen@bandcamp.tv>
2019-01-09 09:34:38 +1100

    type conversion

--- tasks/fix_noop.rb
+++ tasks/fix_noop.rb
@@ -26,11 +26,11 @@ current_state = config['noop']
 
 if target_state != current_state
   _output, stderr, _status = Open3.capture3('puppet config set noop ' + target_state.to_s)
-  if stderr =~ /.*/
+  if stderr.empty?
+    result = 'noop value was ' + current_state + ' it is now ' + target_state.to_s
+  else
     result = stderr
     json['noop_fix'] = 'error encountered'
-  else
-    result = 'noop value was ' + current_state + ' it is now ' + target_state.to_s
   end
 else
   result = 'No action required'
