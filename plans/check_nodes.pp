71c10a690b768b6c87f5b35c04ea0a706323db91
Tony Green <tgreen@bandcamp.tv>
2019-01-09 10:23:30 +1100

    error checking

--- plans/check_nodes.pp
+++ plans/check_nodes.pp
@@ -1,5 +1,15 @@
 plan puppet_health_check::check_nodes(
   TargetSpec  $nodes,
 ) {
-  return run_task('puppet_health_check::agent_health', $nodes)
+
+  get_targets($nodes).each |$node| {
+    $r = run_task('puppet_health_check::agent_health', $node, '_catch_errors' => true)
+    $r.each |$result| {
+      if $result.ok {
+        notice("${node} returned a value: ${result.value}")
+      } else {
+        notice("${node} errored with a message: ${result.error.message}")
+      }
+    }
+  }
 }
