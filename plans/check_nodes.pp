73b9f357b7fc1e84af5f121b35f4a638e9948dd1
Tony Green <tgreen@bandcamp.tv>
2019-01-09 11:01:07 +1100

    Bring changes into the repo

--- plans/check_nodes.pp
+++ plans/check_nodes.pp
@@ -5,9 +5,17 @@ plan puppet_health_check::check_nodes(
   get_targets($nodes).each |$node| {
     $r = run_task('puppet_health_check::agent_health', $node, '_catch_errors' => true)
     $r.each |$result| {
-      $response = $result.value
+     $response = $result.value
       if $response['noop'] {
         notice('Noop set - fixing')
+        $noop = run_task('puppet_health_check::fix_noop', $node, '_catch_errors' => true)
+        $noop.each | $noop_result | {
+          if $noop_result.ok {
+            notice("Fixed noop on ${node}")
+          } else {
+            notice("${node} errored with a message: ${noop_result.value}")
+          }
+        }
       }
       if $result.ok {
         notice("${node} returned a value: ${result.value}")
